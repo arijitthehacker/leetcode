307a3fb160062b380095beabb702413d5c93be19
arijitthehacker <senarijit1618@gmail.com>
2024-04-22 12:45:52 +0530

    feat: concepts

---
+++ concepts/data_structures/array_list.py
@@ -0,0 +1,23 @@
+'''Array All concepts
+'''
+
+
+def create_array():
+    return [int(i) for i in input().strip().split()]
+
+def delete_element():
+    return int(input().strip())
+
+def insert_element():
+    return [int(i) for i in input().strip().split()]
+
+def update_element():
+    return [int(i) for i in input().strip().split()]
+
+
+
+# if __name__ == '__main__':
+#     # n = int(input())
+#     arr = create_array()
+#     delete_element()
+#     print(arr)
